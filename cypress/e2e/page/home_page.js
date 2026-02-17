2808eabd94bfac56969392ce331349eb1bb727cf
Guilherme Campos <157999298+guilhermecampos-dev@users.noreply.github.com>
2026-02-17 16:10:24 -0300

    feat: implement home page actions and business logic

---
+++ cypress/e2e/page/home_page.js
@@ -0,0 +1,14 @@
+import HomeElements from "../elements/home_elements";
+const homeElements = new HomeElements();
+
+class HomePage{
+
+    accessBroworkoutPage(){
+        cy.visit("/")
+    }
+
+    accessExercisesPage(){
+        cy.get(homeElements.exercisesLink()).first.click()
+    }
+
+}export default HomePage;
