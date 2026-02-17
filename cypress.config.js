56b7de9c5265a1c0dfe67a768a91f322006a2f33
Guilherme Campos <157999298+guilhermecampos-dev@users.noreply.github.com>
2026-02-17 15:23:38 -0300

    feat: setup cypress architecture and home page elements

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { defineConfig } = require("cypress");
+const {addCucumberPreprocessorPlugin} = require("@badeball/cypress-cucumber-preprocessor");
+const {createEsbuildPlugin} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "https://bro-workout-frontend.vercel.app",
+    specPattern:"cypress/e2e/features/**/*.feature",
+    async setupNodeEvents(on, config){
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
